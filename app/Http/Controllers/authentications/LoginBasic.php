ce6ed95f1a7ff2fe7d64f1bea4884a74b8a98f6c
deepak.de84 <deepakbhosale20@gmail.com>
2025-09-08 14:35:09 +0530

    login module

---
+++ app/Http/Controllers/authentications/LoginBasic.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\authentications;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; // ✅ add this
+
+class LoginBasic extends Controller
+{
+  public function index()
+  {
+    $pageConfigs = ['myLayout' => 'blank'];
+    return view('content.authentications.auth-login-basic', ['pageConfigs' => $pageConfigs]);
+  }
+
+  public function login(Request $request)
+  {
+
+    return redirect()->intended(route('dashboard-analytics'));
+
+    echo "<pre>";
+    print_r($request->all());
+    die;
+
+    $request->validate([
+      'email' => 'required|string', // can be email or username
+      'password' => 'required|string',
+    ]);
+
+    echo "here";
+    die;
+
+
+    $loginField = $request->input('email');
+
+    // check if input is an email or username
+    $fieldType = filter_var($loginField, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+    $credentials = [
+      $fieldType => $loginField,
+      'password' => $request->input('password'),
+    ];
+    print_r($credentials);
+    die;
+
+
+    if (Auth::attempt($credentials, $request->filled('remember'))) {
+      $request->session()->regenerate();
+      return redirect()->intended(route('dashboard-analytics'));
+    }
+
+    return back()->withErrors([
+      'email' => 'Invalid credentials.',
+    ])->withInput();
+  }
+
+
+  public function logout(Request $request)
+  {
+    Auth::logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+
+    return redirect()->route('login');
+  }
+}
---
+++ resources/views/content/authentications/auth-login-basic.blade.php
@@ -0,0 +1,85 @@
+@php
+  $customizerHidden = 'customizer-hide';
+@endphp
+
+@extends('layouts/layoutMaster')
+
+@section('title', 'Login - Elfinic')
+
+@section('vendor-style')
+  @vite(['resources/assets/vendor/libs/@form-validation/form-validation.scss'])
+@endsection
+
+@section('page-style')
+  @vite(['resources/assets/vendor/scss/pages/page-auth.scss'])
+@endsection
+
+@section('vendor-script')
+  @vite(['resources/assets/vendor/libs/@form-validation/popular.js', 'resources/assets/vendor/libs/@form-validation/bootstrap5.js', 'resources/assets/vendor/libs/@form-validation/auto-focus.js'])
+@endsection
+
+@section('page-script')
+  @vite(['resources/assets/js/pages-auth.js'])
+@endsection
+
+@section('content')
+  <div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+      <div class="authentication-inner">
+        <!-- Login -->
+        <div class="card px-sm-6 px-0">
+          <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center">
+              <a href="index.html" class="app-brand-link gap-2">
+                <span class="app-brand-logo demo">
+                  <span class="text-primary">
+                    <img src="assets/img/elfinic-logo.png" alt="elfinic logo" style="width: 100%;">
+                  </span>
+                </span>
+              </a>
+            </div>
+            <!-- /Logo -->
+            <h4 class="mb-1">Welcome to {{ config('variables.templateName') }}! 👋</h4>
+            <p class="mb-6">Please sign-in to your account and start the adventure</p>
+
+            <form id="formAuthentication" class="mb-6" action="{{ route('login.perform') }}" method="POST">
+              @csrf
+              <div class="mb-6 form-control-validation">
+                <label for="email" class="form-label">Email or Username</label>
+                <input type="text" class="form-control" id="email" name="email"
+                  placeholder="Enter your email or username" autofocus />
+              </div>
+              <div class="mb-6 form-password-toggle form-control-validation">
+                <label class="form-label" for="password">Password</label>
+                <div class="input-group input-group-merge">
+                  <input type="password" id="password" class="form-control" name="password"
+                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                    aria-describedby="password" />
+                  <span class="input-group-text cursor-pointer"><i class="icon-base bx bx-hide"></i></span>
+                </div>
+              </div>
+              <div class="mb-7">
+                <div class="d-flex justify-content-between">
+                  <div class="form-check mb-0">
+                    <input class="form-check-input" type="checkbox" id="remember-me" />
+                    <label class="form-check-label" for="remember-me"> Remember Me </label>
+                  </div>
+                  <a href="{{ url('auth/forgot-password-basic') }}">
+                    <span>Forgot Password?</span>
+                  </a>
+                </div>
+              </div>
+              <div class="mb-6">
+                <button class="btn btn-primary d-grid w-100" type="submit">Login</button>
+              </div>
+            </form>
+
+
+          </div>
+        </div>
+        <!-- /Login -->
+      </div>
+    </div>
+  </div>
+@endsection
