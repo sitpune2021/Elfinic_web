d7f4cba4af1211742c9f2b30b33c5bef734447ea
sagardada <sagarchavan219@gmail.com>
2025-09-16 14:54:55 +0530

    Todays Updated code1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Models\Category;   // Import Category model
+use App\Models\Product;   // Import Product model
 
 Route::get("/getCategories", function () {
 
@@ -11,5 +12,17 @@ Route::get("/getCategories", function () {
     return response()->json([
         'status' => true,
         'data'   => $categories
+    ]);
+});
+
+Route::get("/getProducts", function () {
+
+    $products = Product::where('status', 'active')
+                          ->orderBy('id', 'desc')
+                          ->get();
+//echo "<pre>";print_r($products);die;
+    return response()->json([
+        'status' => true,
+        'data'   => $products
     ]);
 });
